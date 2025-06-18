54187f492bde38c298523962ec75c1a7fa9b9a17
frahman2025 <fazlur.Rahman@ossm.edu>
2025-06-18 10:39:05 -0500

    Create and.v

---
+++ src/and.v
@@ -0,0 +1,16 @@
+/*
+A simple 8-bit AND in verilog,TOP
+*/
+
+module tt_um_and(
+  input clk, 
+  input reset,
+  input[7:0] a, b,
+  output [7:0] Y
+);
+
+  assign Y = a & b; 
+
+
+
+    endmodule 
